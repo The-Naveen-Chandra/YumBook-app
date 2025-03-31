73d1d36a14ad0e81782c2020d0ca2d62047af13d
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2025-04-01 00:34:57 +0530

    Refactor Categories component to use dynamic category data
    - Updated Categories component to accept categories as a prop instead of using a hardcoded constant.
    - Replaced category names and images with dynamic values from the categories prop.
    - Added animation to the component using react-native-reanimated for improved user experience.

--- app/components/categories.tsx
+++ app/components/categories.tsx
@@ -4,49 +4,50 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-
-import { categoryData } from "../constants/constant";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 interface CategoriesProps {
+  categories: any[];
   activeCategory: string;
   setActiveCategory: (category: string) => void;
 }
 
 export default function Categories({
+  categories,
   activeCategory,
   setActiveCategory,
 }: CategoriesProps) {
   return (
-    <View>
+    <Animated.View entering={FadeInDown.duration(500).springify()}>
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
-        {categoryData.map((cat, index) => {
-          let isActive = cat.name === activeCategory;
+        {categories.map((cat, index) => {
+          let isActive = cat.strCategory === activeCategory;
           let activeButtonClass = isActive ? "bg-orange-400" : "bg-black/10";
 
           return (
             <TouchableOpacity
               key={index}
-              onPress={() => setActiveCategory(cat.name)}
+              onPress={() => setActiveCategory(cat.strCategory)}
               className="flex items-center mr-4"
             >
               <View className={"rounded-full p-[6px] " + activeButtonClass}>
                 <Image
-                  source={{ uri: cat.image }}
+                  source={{ uri: cat.strCategoryThumb }}
                   style={{ width: hp(6), height: hp(6) }}
                   className="rounded-full"
                 />
               </View>
               <Text className="text-neutral-600" style={{ fontSize: hp(1.6) }}>
-                {cat.name}
+                {cat.strCategory}
               </Text>
             </TouchableOpacity>
           );
         })}
       </ScrollView>
-    </View>
+    </Animated.View>
   );
 }
